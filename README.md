ad59aae426ee70c9738360b33132c7f863e0e59d
awsome-webdev <awsome-webdev@users.noreply.github.com>
2025-01-21 23:09:42 +0000

    deploy: be63cb66f631d6acda677198937092c269b482f2

---
+++ README.md
@@ -0,0 +1,17 @@
+<h1>Nooby</h1>
+<p>Nooby is a unblocked games site and also has proxies, hacks, and some of my other projects. It has a very low quality chatroom (at least it works)</p>
+
+<a href="https://some-noob.vercel.app">Another link to the site!!</a>
+Major contributor!
+<a href="https://github.com/ekoerp1" target="_blank" >ekoerp1</a>
+thank you!!
+
+Want to contribute? Email me for instructions (if your adding hacks or games!)
+## Star History
+
+[![Star History Chart](https://api.star-history.com/svg?repos=bcrhbrhcdb/Nooby&type=Date)](https://star-history.com/#bcrhbrhcdb/Nooby&Date)
+
+The site is growing (real)
+Visitors currently:
+
+<div align="center"><a href="https://www.free-website-hit-counter.com"><img src="https://www.free-website-hit-counter.com/zc.php?d=9&amp;id=1109&amp;s=2" border="0" alt=""></a><br><small><a href="https://www.free-website-hit-counter.com" title="Free Website Hit Counter"></a></small></div>
---
+++ javascript/movieData.js
@@ -0,0 +1,40 @@
+const movies = {
+    deadPoolAndWolve: {
+        title: "Deadpool & Wolverine",
+        url: "movie-template.html?id=deadPoolAndWolve",
+        originalUrl: "https://drive.google.com/file/d/144etb6-SFvWTl89zkwfAEMNcZFYejsE_/preview",
+        image: "../images/movie-images/deadpool-wolverine.jpeg",
+        description: "Made in 2024, haven't watched and won't spoil the movie!",
+        ratings: [],
+        likes: 0,
+        dislikes: 0,
+    },
+    thelegomovie: {
+        title: "The Lego Movie",
+        url: "movie-template.html?id=thelegomovie",
+        originalUrl: "https://drive.google.com/file/d/1m5E6o5BU7hiDlJMmGbwt8ipcETxk4MJQ/preview",
+        image: "../images/movie-images/thelegomovie.jpeg",
+        description: "Made in 2014, A classic!",
+        ratings: [],
+        likes: 0,
+        dislikes: 0,
+    },
+     fnafmovie: {
+        title: "The FNAF Movie",
+        url: "movie-template.html?id=fnafmovie",
+        originalUrl: "https://ia801701.us.archive.org/10/items/fnafmovie_20231102/downloadfile.mp4",
+        image: "../images/movie-images/fnafmovie.jpeg",
+        description: "Made in 2023, Pretty good movie!",
+        ratings: [],
+        likes: 0,
+        dislikes: 0,
+    },
+    minionsGru: {
+        title: "Minions, Rise of Gru",
+        url: "movie-template.html?id=minionsGru",
+        originalUrl: "https://drive.google.com/file/d/1XNolpeeosJbfgYnfknKf1OM7bwHhqibl/preview",
+        image: "../images/movie-images/minions-gru.jpeg",
+        description: "banana"
+    }
+}
+
